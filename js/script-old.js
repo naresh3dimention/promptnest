0c5a646dde266b97ccc2c054cdabbb30500ff029
Abhinow Cheruku <naresh3dimention@gmail.com>
2026-07-31 11:47:21 +0530

    Fix Dark Mode and Mobile Menu

--- js/script-old.js
+++ js/script-old.js
@@ -246,3 +246,17 @@ if(menuToggle && navLinks){
     });
 
 }
+// =========================
+// Mobile Menu
+// =========================
+
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.querySelector(".nav-links");
+
+if (menuToggle && navLinks) {
+
+    menuToggle.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
+
+}
